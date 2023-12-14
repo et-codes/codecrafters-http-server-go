b94eec8614f036327666fa4c6e86b4d70ca09bc9
et-codes <ericthornton43@gmail.com>
2023-12-14 02:24:21 +0000

    Add Server

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/codecrafters-io/http-server-starter-go/logging"
+)
+
+const (
+	host = "localhost"
+	port = "4221"
+)
+
+var logger = logging.New(logging.LevelDebug)
+
+func main() {
+	s := NewServer(host, port)
+	if err := s.Listen(); err != nil {
+		logger.Fatal("Error starting server: %v", err)
+	}
+
+	logger.Info("Listening on port %s", port)
+}
--- app/server.go
+++ app/server.go
@@ -3,19 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+type Server struct {
+	host string
+	port string
+}
+
+func NewServer(host, port string) *Server {
+	return &Server{host, port}
+}
+
+func (s *Server) Listen() error {
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", host, port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		logger.Error("Failed to bind to port %s", port)
+		return err
 	}
-	
+
 	_, err = l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		logger.Error("Error accepting connection: ", err.Error())
+		return err
 	}
+	return nil
 }
