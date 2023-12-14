94c0503799c1f9f039e3b58370ba8174238f0ca6
et-codes <ericthornton43@gmail.com>
2023-12-14 03:51:53 +0000

    Stage 4

--- app/handler.go
+++ app/handler.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"net"
 	"strings"
 )
 
 const (
-	respOK       = "HTTP/1.1 200 OK\r\n\r\n"
-	respNotFound = "HTTP/1.1 404 Not Found\r\n\r\n"
+	CRLF           = "\r\n"
+	delim          = "\r\n\r\n"
+	respOK         = "HTTP/1.1 200 OK\r\n\r\n"
+	respNotFound   = "HTTP/1.1 404 Not Found\r\n\r\n"
+	statusOK       = "HTTP/1.1 200 OK"
+	statusNotFound = "HTTP/1.1 404 Not Found"
+	textPlain      = "text/plain"
 )
 
 type Handler struct {
@@ -23,27 +28,59 @@ func (h *Handler) Start() {
 	logger.Info("Handler invoked.")
 	defer h.conn.Close()
 
-	scanner := bufio.NewScanner(h.conn)
-
-	scanner.Scan()
-	response := scanner.Text()
-	logger.Debug(response)
-
-	parts := strings.Split(response, " ")
-	path := parts[1]
-
-	switch path {
-	case "/":
-		_, err := h.conn.Write([]byte(respOK))
-		if err != nil {
-			logger.Error("Error writing response: %v", err)
-			return
-		}
-	default:
-		_, err := h.conn.Write([]byte(respNotFound))
-		if err != nil {
-			logger.Error("Error writing response: %v", err)
-			return
-		}
+	// Get
+	request, err := h.getRequest()
+	if err != nil {
+		logger.Error("Error reading response: %v", err)
+		return
 	}
+
+	lines := strings.Split(request, CRLF)
+	startLine := lines[0]
+	logger.Info(startLine)
+
+	words := strings.Split(startLine, " ")
+	method, path := words[0], words[1]
+
+	if method != "GET" {
+		logger.Warning("Unsupported method %s", method)
+	}
+
+	var response []byte
+	switch {
+	case path == "/":
+		response = []byte(respOK)
+		_, err = h.conn.Write(response)
+	case path[:6] == "/echo/":
+		message := path[6:]
+		response = newResponse(statusOK, textPlain, message)
+		_, err = h.conn.Write(response)
+	default:
+		response = []byte(respNotFound)
+		_, err = h.conn.Write(response)
+	}
+	if err != nil {
+		logger.Error("Error writing reply: %v", err)
+		return
+	}
+	logger.Debug("reply sent.\n%s", response)
+}
+
+func (h *Handler) getRequest() (string, error) {
+	response := make([]byte, 1024)
+	_, err := h.conn.Read(response)
+	if err != nil {
+		return "", fmt.Errorf("error reading response: %v", err)
+	}
+	return string(response), nil
+}
+
+func newResponse(status, contentType, body string) []byte {
+	return []byte(fmt.Sprintf(
+		"%s\r\n%s: %s\r\n%s: %d\r\n\r\n%s",
+		status,
+		"Content-Type", contentType,
+		"Content-Length", len(body),
+		body,
+	))
 }
