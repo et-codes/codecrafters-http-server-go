1f9b8b7eed181a84709c84dd5bcd098e4f3e1a90
et-codes <ericthornton43@gmail.com>
2023-12-14 02:15:12 +0000

    Stage 1

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Configure GitHub identity
+git config --global user.email "ericthornton43@gmail.com"
+git config --global user.name "et-codes"
+
+# Install codecrafters CLI
+curl https://codecrafters.io/install.sh | sh
