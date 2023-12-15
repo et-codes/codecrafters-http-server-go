fefbf7eb554e39c78b5c715e75d228c78a1805e6
et-codes <ericthornton43@gmail.com>
2023-12-15 03:34:18 +0000

    Update logger

--- logging/logging.go
+++ logging/logging.go
@@ -16,42 +16,64 @@ const (
 
 // logging.New(l int) returns a new logger initialized with the level set to l.
 // Use the logger.LevelX constants to set the level.
-func New(l int) *Logger {
-	if l < LevelDebug {
-		l = LevelDebug
+func New(level int) *Logger {
+	if level < LevelDebug {
+		level = LevelDebug
 	}
-	return &Logger{Level: l}
+	return &Logger{Level: level}
 }
 
 // Debug messages are shown if level is set to LevelDebug.
 func (l *Logger) Debug(s string, args ...any) {
 	if l.Level <= LevelDebug {
-		log.Printf("DEBUG "+s, args...)
+		if len(args) == 0 {
+			log.Println("DEBUG " + s)
+		} else {
+			log.Printf("DEBUG "+s, args...)
+		}
 	}
 }
 
 // Info messages are shown if level is set to LevelInfo or lower.
 func (l *Logger) Info(s string, args ...any) {
 	if l.Level <= LevelInfo {
-		log.Printf("INFO "+s, args...)
+		if len(args) == 0 {
+			log.Println("INFO " + s)
+		} else {
+			log.Printf("INFO "+s, args...)
+		}
 	}
 }
 
 // Warning messages are shown if level is set to LevelWarning or lower.
 func (l *Logger) Warning(s string, args ...any) {
 	if l.Level <= LevelWarning {
-		log.Printf("WARNING "+s, args...)
+		if len(args) == 0 {
+			log.Println("WARNING " + s)
+		} else {
+			log.Printf("WARNING "+s, args...)
+		}
 	}
 }
 
 // Error messages are shown if level is set to LevelError or lower.
 func (l *Logger) Error(s string, args ...any) {
 	if l.Level <= LevelError {
-		log.Printf("ERROR "+s, args...)
+		if l.Level <= LevelWarning {
+			if len(args) == 0 {
+				log.Println("ERROR " + s)
+			} else {
+				log.Printf("ERROR "+s, args...)
+			}
+		}
 	}
 }
 
 // Fatal messages are always shown.
 func (l *Logger) Fatal(s string, args ...any) {
-	log.Fatalf("FATAL "+s, args...)
+	if len(args) == 0 {
+		log.Fatalln("FATAL " + s)
+	} else {
+		log.Fatalf("FATAL "+s, args...)
+	}
 }
